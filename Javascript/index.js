cc56b65de9c1b96482bfca972fb1a6194ce76873
Shridhar <shridharcbe@gmail.com>
2025-12-13 12:01:51 +0530

    Simple Program Counter

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,9 +1,19 @@
-const PI=3.14159;
+let decrease=document.getElementById("decrease");
+let increase=document.getElementById("increase");
+let reset=document.getElementById("reset");
+let l=document.getElementById("l");
+let count=0;
 
-
-document.getElementById("MyButton").onclick=function(){
-    let t=document.getElementById("ra").value;
-    t=Number(t);
-    let a=PI*t*t;
-    document.getElementById("h11").textContent="Area of Circle is: "+a;
+decrease.onclick=function(){
+    count--;
+    document.getElementById("l").textContent=count;
 }
+increase.onclick=function(){
+    count++;
+    document.getElementById("l").textContent=count;
+}
+reset.onclick=function(){
+    count=0;
+    document.getElementById("l").textContent=count;
+}
+
