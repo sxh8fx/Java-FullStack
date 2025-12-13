80c26e1b6e8da3396f0f959ee2ffcd25fbb61a51
Shridhar <shridharcbe@gmail.com>
2025-12-13 10:15:59 +0530

    JS Variables

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,20 +1,16 @@
-let x=123;
-//console.log(x);
-let age=25;
-//console.log(age);
-let gpa=8.1;
-let mrp=12.95;
-let n="Shridhar";
-console.log(`Youre gpa is ${gpa}`); //Use backtick
-console.log(`You're age is ${age}`);
-console.log(`Price is: $${mrp}`); 
-console.log(typeof n);
-let forSale =true; //Boolean
-console.log(`This Car is for Sale: ${forSale}`);
-let fullName ="Shridhar";
-let a =21;
-let s=true;
-document.getElementById("p1").textContent=`My full name is ${fullName}`;
-document.getElementById("p2").textContent=`I'm age is ${a}`;
-document.getElementById("p3").textContent=`I am Student: ${s}`;
-
+// Arithmetic Operator
+ let film=100;
+ film+=1;
+ film-=1;
+ film*=2;
+ film/=2;
+ film++;
+ film--;
+ let exp=film**3;
+ console.log(exp);
+ /* Operator Precedence
+ 1. Paranthesis()
+ 2. Exponents
+ 3. Multiplication, Division , Modulo
+ 4. Addition and Subtraction
+ */
