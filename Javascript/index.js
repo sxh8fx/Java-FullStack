7ec6ab102d4191379996f10f0f291c50cdc13517
Shridhar <shridharcbe@gmail.com>
2025-12-13 10:02:01 +0530

    JS Variables

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,10 +1,20 @@
-// console.log('Hello');
-// console.log('I like Kids');
-// window.alert('This is an Alert');
-
-// This is a comment
-/* This 
-is a multi line comment */
-document.getElementById("h").textContent="Hello";
-document.getElementById("p").textContent="I like Kids";
+let x=123;
+//console.log(x);
+let age=25;
+//console.log(age);
+let gpa=8.1;
+let mrp=12.95;
+let n="Shridhar";
+console.log(`Youre gpa is ${gpa}`); //Use backtick
+console.log(`You're age is ${age}`);
+console.log(`Price is: $${mrp}`); 
+console.log(typeof n);
+let forSale =true; //Boolean
+console.log(`This Car is for Sale: ${forSale}`);
+let fullName ="Shridhar";
+let a =21;
+let s=true;
+document.getElementById("p1").textContent=`My full name is ${fullName}`;
+document.getElementById("p2").textContent=`I'm age is ${a}`;
+document.getElementById("p3").textContent=`I am Student: ${s}`;
 
