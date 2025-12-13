79ee69b2742c54a76787a8f5f3838e611d60dd40
Shridhar <shridharcbe@gmail.com>
2025-12-13 09:37:03 +0530

    JS Basics

---
+++ Javascript/index.js
@@ -0,0 +1,10 @@
+// console.log('Hello');
+// console.log('I like Kids');
+// window.alert('This is an Alert');
+
+// This is a comment
+/* This 
+is a multi line comment */
+document.getElementById("h").textContent="Hello";
+document.getElementById("p").textContent="I like Kids";
+
